6ad5213f9a1651cfb0e2195ebcbb83aa33ef0263
Nikita <niknakels@gmail.com>
2024-06-08 02:18:33 +1100

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+# Практическое задание по уроку "Строки и индексация строк"
+example = 'Синтаксис'
+
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1::2])
